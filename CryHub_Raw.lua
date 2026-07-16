37b8522d89d16657bc7dfe0699ecec23a83f4f4b
m4s78ccngm-cell <m4s78ccngm@privaterelay.appleid.com>
2026-07-16 03:20:42 +0300

    Add raw loadstring version for CryHub script

---
+++ CryHub_Raw.lua
@@ -0,0 +1,4 @@
+-- CryHub Raw Loadstring Version
+-- Usage: loadstring(game:HttpGet("https://raw.githubusercontent.com/m4s78ccngm-cell/Cry-hub/main/CryHub_Raw.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/m4s78ccngm-cell/Cry-hub/main/Cry%20hub"))()
